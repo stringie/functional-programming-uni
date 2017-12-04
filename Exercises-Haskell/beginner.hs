d84b5aa20af9cd301c9926fbd3be885791c1c424
stringie <antony3620@gmail.com>
2017-12-05 00:29:40 +0200

    exercised.

--- Exercises-Haskell/beginner.hs
+++ Exercises-Haskell/beginner.hs
@@ -1,8 +1,6 @@
-quicksort :: (Ord a) => [a] -> [a]
-quicksort [] = []
-quicksort (x:xs) = smaller ++ [x] ++ larger
-    where smaller = quicksort [a | a <- xs, a <= x]
-          larger = quicksort [a | a <- xs, a > x]
+oddSquareSum :: Integer
+oddSquareSum = sum $ takeWhile (<1000) $ filter odd $ map (^2) [1..]
+
 
 main = do
-    print (quicksort [5,6,3,4,6,7,8,9,1,2,3,2,4,6,7,4])
+    print $ oddSquareSum
