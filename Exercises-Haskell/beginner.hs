03b94ca1c69222b39cd68717caeedef44fa086c6
stringie <antony3620@gmail.com>
2017-12-04 11:23:41 +0200

    testing haskell

--- Exercises-Haskell/beginner.hs
+++ Exercises-Haskell/beginner.hs
@@ -1,6 +1,8 @@
-factoriel :: Integer -> Integer
-factoriel n = product [1..n]
-
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = smaller ++ [x] ++ larger 
+    where smaller = quicksort [a | a <- xs, a <= x]
+          larger = quicksort [a | a <- xs, a > x]
 
 main = do
-    print (factoriel 2)
+    print (quicksort [6, 4, 2, 7, 8, 4, 6, 4, 1, 9, 3, 4, 5, 6])
