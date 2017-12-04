42bed630c939f9da6769d6dadff806411f6d1c28
stringie <antony3620@gmail.com>
2017-12-04 12:01:59 +0200

    nofin

--- Exercises-Haskell/beginner.hs
+++ Exercises-Haskell/beginner.hs
@@ -1,8 +1,8 @@
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
-quicksort (x:xs) = smaller ++ [x] ++ larger 
+quicksort (x:xs) = smaller ++ [x] ++ larger
     where smaller = quicksort [a | a <- xs, a <= x]
           larger = quicksort [a | a <- xs, a > x]
 
 main = do
-    print (quicksort [6, 4, 2, 7, 8, 4, 6, 4, 1, 9, 3, 4, 5, 6])
+    print (quicksort [5,6,3,4,6,7,8,9,1,2,3,2,4,6,7,4])
