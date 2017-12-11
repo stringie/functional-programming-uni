f1d5233d1ac6a0852fb3fd790e53113ab9f02f7a
stringie <antony3620@gmail.com>
2017-12-12 01:05:56 +0200

    exercisin

--- Exercises-Haskell/test.hs
+++ Exercises-Haskell/test.hs
@@ -1,3 +1,21 @@
-
+import System.IO  
+import System.Directory  
+import Data.List  
 
 main = do
+    handle <- openFile "todo.txt" ReadMode
+    (tempName, tempHandle) <- openTempFile "." "temp"
+    contents <- hGetContents handle
+    let tasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] tasks
+    putStrLn "These are your todos:"
+    putStr $ unlines numberedTasks
+    putStrLn "What will you delete?"
+    numberString <- getLine
+    let number = read numberString
+        newTodo = delete (tasks !! number) tasks
+    hPutStr tempHandle $ unlines newTodo
+    hClose handle
+    hClose tempHandle
+    removeFile "todo.txt"
+    renameFile tempName "todo.txt"
