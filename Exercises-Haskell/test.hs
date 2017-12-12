11635df126f635aeb6f4a63125cbec227946dcdb
stringie <antony3620@gmail.com>
2017-12-12 14:24:39 +0200

    heathrow

--- Exercises-Haskell/test.hs
+++ Exercises-Haskell/test.hs
@@ -1,9 +1,35 @@
-solveRPN :: (Num a, Read a) => String -> a
-solveRPN = head . foldl folding [] . words
-    where folding (x:y:ys) "+" = (x+y):ys
-          folding (x:y:ys) "-" = (x-y):ys
-          folding (x:y:ys) "*" = (x*y):ys
-          folding xs numberString = read numberString:xs
+data Section = Section {getA :: Int, getB :: Int, getC :: Int} deriving (Show)
+type RoadSystem = [Section]
+
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
+
+heathrowToLondon :: RoadSystem  
+heathrowToLondon = [Section 50 10 30, Section 5 90 20, Section 40 2 25, Section 10 8 0]
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)  
+roadStep (pathA, pathB) (Section a b c) =   
+    let priceA = sum $ map snd pathA  
+        priceB = sum $ map snd pathB  
+        forwardPriceToA = priceA + a  
+        crossPriceToA = priceB + b + c  
+        forwardPriceToB = priceB + b  
+        crossPriceToB = priceA + a + c  
+        newPathToA = if forwardPriceToA <= crossPriceToA  
+                        then (A,a):pathA  
+                        else (C,c):(B,b):pathB  
+        newPathToB = if forwardPriceToB <= crossPriceToB  
+                        then (B,b):pathB  
+                        else (C,c):(A,a):pathA  
+    in  (newPathToA, newPathToB)  
+
+optimalPath :: RoadSystem -> Path  
+optimalPath roadSystem = 
+    let (bestAPath, bestBPath) = foldl roadStep ([],[]) roadSystem  
+    in  if sum (map snd bestAPath) <= sum (map snd bestBPath)  
+            then reverse bestAPath  
+            else reverse bestBPath  
+
 main = do
-    print $ solveRPN "10 20 +"
+    print $ optimalPath heathrowToLondon
     
