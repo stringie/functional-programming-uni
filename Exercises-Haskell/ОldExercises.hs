8ef757bf3597f3d3ea57f2dd1e89ba1c7a4d57b0
stringie <antony3620@gmail.com>
2017-12-12 23:09:20 +0200

    more exercising

--- Exercises-Haskell/ОldExercises.hs
+++ Exercises-Haskell/ОldExercises.hs
@@ -1,12 +1,15 @@
-sort :: [Integer] -> [Integer]
-sort [] = []
-sort (x:xs) = smaller ++ [x] ++ larger
-    where smaller = sort $ filter (\y -> y <= x) xs
-          larger = sort $ filter (\y -> y > x) xs
+import Data.List
+import Data.Function
 
-bc :: (Floating a, Eq a) => a -> a -> a
-bc n 0 = 1
-bc n k = (n / k) * (bc (n - 1) (k - 1))
+qsort :: [Integer] -> [Integer]
+qsort [] = []
+qsort (x:xs) = smaller ++ [x] ++ larger
+    where smaller = qsort $ filter (\y -> y <= x) xs
+          larger = qsort $ filter (\y -> y > x) xs
+
+choose :: (Floating a, Eq a) => a -> a -> a
+n `choose` 0 = 1
+n `choose` k = (n / k) * ((n - 1) `choose` (k - 1))
 
 intToList :: Integer -> [Integer]
 intToList 0 = []
@@ -30,5 +33,40 @@ isPerfectNumber n = (==) n $ sum $ filter (\x -> n `mod` x == 0) [1..(n - 1)]
 hasAscendingDigits :: Integer -> Bool
 hasAscendingDigits n = intToList n == (sort $ intToList n)
 
+bullsAndCows :: Integer -> Integer -> (Int, Int)
+bullsAndCows a b = (bulls, cows)
+    where bulls = length $ filter (\x -> fst x == snd x) $ zip (intToList a) (intToList b)
+          cows  = (length $ filter (\x -> x `elem` (intToList b)) (intToList a)) - bulls
+
+countMinimum :: [Integer] -> Int
+countMinimum xs = length $ filter (==(minimum xs)) xs
+
+getPowerSet :: [Integer] -> [[Integer]]
+getPowerSet = sortOn length . nub . concat . map inits . tails
+
+getSublists :: [Integer] -> [[Integer]]
+getSublists = sortOn length . concat . map inits . tails
+
+countOccurances :: [Integer] -> [Integer] -> Int
+countOccurances xs subxs = length $ filter (==subxs) (getSublists xs)
+
+sieveOfEratosthenes :: Integer -> [Integer]
+sieveOfEratosthenes n = sieve [2..n]
+    where sieve [] = []  
+          sieve (x:xs) = x : (sieve $ filter (\e -> e `mod` x /= 0) xs)
+
+getMaxPower :: Integer -> Integer -> Integer
+getMaxPower number base = helper 0
+    where helper power = if number `mod` base^power /= 0 then power - 1 else helper (power + 1)
+
+primeFactors :: Integer -> [(Integer, Integer)]
+primeFactors n = map (\f -> (f, getMaxPower n f)) . filter (\x -> n `mod` x == 0) $ sieveOfEratosthenes n
+
+dot :: (Num a) => [a] -> [a] -> a
+v `dot` u = sum $ zipWith (*) v u 
+
+sumUnique :: [[Integer]] -> Integer
+sumUnique xxs = sum . concat . filter ((==1) . length) . concat . map (group . sort) $ xxs
+
 main = do 
-    print $ hasAscendingDigits 121
+    print $ replicate 10 (1,1)
